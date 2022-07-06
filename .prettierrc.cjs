5d8d60ef3c44aa1fd6e24c37b5f99b6e5e4d82a7
jonz94 <jody16888@gmail.com>
2022-07-06 17:18:01 +0800

    build: change to pnpm

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,13 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   arrowParens: 'always',
   printWidth: 120,
   semi: true,
   singleQuote: true,
   trailingComma: 'all',
+
+  plugins: [require('prettier-plugin-packagejson'), require('prettier-plugin-organize-imports')],
+
   overrides: [
     {
       files: '*.json',
